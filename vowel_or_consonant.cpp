517e234755c801e1d7269845bdea1503d1c509f1
Manish Singh <48895984+MSKU786@users.noreply.github.com>
2019-05-06 14:58:19 +0530

    Add files via upload

---
+++ vowel_or_consonant.cpp
@@ -0,0 +1,13 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    cin>>c;
+    c=tolower(c);
+    if(c=='a'||c=='e'||c=='o'||c=='i'||c=='u')
+        cout<<"Vowel";
+    else
+        cout<<"Consonant";
+}
